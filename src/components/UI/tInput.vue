ec8ab7c4b39b7e812ff671d8e795742433d092a2
Kolhan <kolhan@mail.ru>
2022-06-08 13:37:10 +0500

    add: page template

---
+++ src/components/UI/tInput.vue
@@ -0,0 +1,11 @@
+<template>
+    <input 
+        type="text" 
+        class="border border-gray-400 block w-full py-1 px-2" 
+        value="test" 
+    />
+</template>
+
+<script setup lang="ts">
+    
+</script>
